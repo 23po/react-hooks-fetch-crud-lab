298da81095cbcbfb984181ec460f1fcdc542266d
calvin <ckodalo@gmail.com>
2022-09-29 00:46:43 +0300

    fetch list and display successful

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,16 @@
 import React from "react";
+import QuestionItem from  "./QuestionItem"; 
 
-function QuestionList() {
+function QuestionList({questions}) {
+  console.log(questions)
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map( (question) => { 
+        console.log(question)
+        return <QuestionItem key = {question.id} question = {question} />
+      }
+      )}</ul>
     </section>
   );
 }
